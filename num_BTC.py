7ce12461ee8081cc6a4582ff6d1739bed27f3259
Dan Luo <luo_dana@yahoo.com>
2023-02-27 20:23:58 +1000

    Update num_BTC.py

--- num_BTC.py
+++ num_BTC.py
@@ -6,13 +6,15 @@ def num_BTC(b):
     reward_token = 50
     halves = (b-1)//210000
     total_reward = 0
-    for i in range(0,halves):
+    for i in range(0,halves):                
+        
+        total_reward = reward_token*210000 + total_reward
         reward_token = reward_token/2
-        total_reward = reward_token*210000
+        #print(total_reward)
     remaining_block = b - halves*210000
-    print(remaining_block)
+    #print(remaining_block)
+    #print(halves)
+    #print(total_reward)
     total_reward = total_reward + remaining_block * (50.0 / (2.0 ** halves))
     c = float(total_reward)
     return c
-
-
